657c4cf32896f0b1710247dbe68cb5d289e43ea3
svelusamy <sugasini@tataelxsi.co.in>
2016-07-19 16:22:10 +0000

    RDKCMF-3974 : Integration of TDK-B in Raspberry Pi platform
    Reason for change: Platform specific tdkb scripts for raspberry pi
    Test Procedure:bitbake rdk-generic-broadband-tdk-image
    Risks: None
    Signed-off-by: svelusamy <svelusamy@tataelxsi.co.in>
    
    Change-Id: I7f7a4ff28b86494e2a746d3afdca760643243fad

---
+++ agent/scripts/StartTDK.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#
+# ============================================================================
+# COMCAST C O N F I D E N T I A L AND PROPRIETARY
+# ============================================================================
+# This file (and its contents) are the intellectual property of Comcast.  It may
+# not be used, copied, distributed or otherwise  disclosed in whole or in part
+# without the express written permission of Comcast.
+# ============================================================================
+# Copyright (c) 2014 Comcast. All rights reserved.
+# ============================================================================
+#
+
+#Setting up environment to run TDK
+export TDK_PATH=/nvram/TDK/
+export PATH=$PATH:/usr/local/bin:$TDK_PATH
+export TDK_LIB_PATH=$TDK_PATH/libs/
+export LD_LIBRARY_PATH=$TDK_PATH/libs/:/usr/local/lib/:/usr/local/Qt/lib/:/mnt/nfs/lib:/mnt/nfs/bin/target-snmp/lib/:/mnt/nfs/bin:/usr/local/lib/sa:$LD_LIBRARY_PATH
+
+export PATH HOME LD_LIBRARY_PATH
+ulimit -c unlimited
+echo "Going to start Agent"
+cd $TDK_PATH/
+sh TDKagentMonitor.sh &
+./rdk_tdk_agent_process
